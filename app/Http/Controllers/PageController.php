c9e2e0f90da4d8b1fb0109842b01ef0ad6b18ec4
israr <misrar09@gmail.com>
2024-01-23 09:23:02 +0100

    created movie model but with db error

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index()
     {
-        $dati = config("data");
-        return view('home', $dati);
+        /*         $dati = config("data"); */
+        $films = Movie::all();
+        return view('home', compact("films"));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da config/data.js: {{ $home }}</h1>
+<h1>Dati da config/data.js: </h1>
+@php
+ var_dump( $films )   
+@endphp
 <p>This is my paragraph content.</p>
 <button class="btn btn-primary">Cliccami</button>
 @endsection
