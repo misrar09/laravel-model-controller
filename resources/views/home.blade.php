a171fcf6c3d76823df3f186234b94956e1981848
israr <misrar09@gmail.com>
2024-01-23 09:47:09 +0100

    printed movie titles successfuly

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Dati da config/data.js: </h1>
-@php
- var_dump( $films )   
-@endphp
-<p>This is my paragraph content.</p>
-<button class="btn btn-primary">Cliccami</button>
+<ul>
+    @foreach ($films as $film)
+        <li>
+            {{$film->title}}
+        </li>
+    @endforeach
+</ul>
+
 @endsection
