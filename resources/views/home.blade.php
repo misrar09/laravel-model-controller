955eda17fb2bdf7abf9dc86cb4539f16cab5358a
israr <misrar09@gmail.com>
2024-01-23 10:06:19 +0100

    updated blade formatting and syntax

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="row">
-    <?php foreach ($films as $film) : ?>
-        <div class="col-3">
-            <div class="card">
-                <div class="card-body">
-                    <h4 class="card-title">
-                    {{$film->title}}
-                    </h4>
-
-                    <h5 class="card-nationality">
-                        {{$film->nationality}}
-                    </h5>
-                    <h6 class="card-date">
-                        {{$film->date}}
-                    </h6>
+    <div class="row">
+        @foreach ($films as $film)
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            {{ $film->title }}
+                        </h4>
+                        <h5 class="card-nationality">
+                            {{ $film->nationality }}
+                        </h5>
+                        <h6 class="card-date">
+                            {{ $film->date }}
+                        </h6>
+                    </div>
                 </div>
             </div>
-        </div>
-    <?php endforeach; ?>
-</div>
-
+        @endforeach
+    </div>
 @endsection
